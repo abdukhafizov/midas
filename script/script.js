eadef05aaab6d1933beeddf72ba0349376f35d95
Abdukhafizov Jamshed <furkatyusupov@MacBook-Air-Furkat.local>
2024-05-01 16:31:20 +0500

    m

--- script/script.js
+++ script/script.js
@@ -4,18 +4,10 @@
 // динамическое отображение категорий
 // категории в ссылках 
 //  при клике например на горячие блюда вышли назыания именно этих блюд а старая штучка удалилась !
-
-let hot_arrow = document.querySelector("#hot_arrow")
-let hot_arrow_box = document.querySelector(".hot_arrow_box")
-// let hotdishes = document.querySelector(".hotdishes")
-// let blackBox = document.querySelector(".blackBox")
-let first_hot_click = document.querySelector(".first_hot_click")
-let about_popular_dishes = document.querySelector(".about_popular_dishes")
-
 let dishes = [
     {
         id: Math.random(),
-        menuImg: "../img/khachapuri.svg",
+        menuImg: "../img/massiveDishes/khachapuri.svg",
         bigImg: "https://cdn.tasteatlas.com/images/dishes/3d92f77889fa49fe9e3ca4c761109e86.jpg?m=facebook",
         name: "Khachapuri in Adjarian",
         title: 'Хачапури "Лодочка" с начинкой из расплавленного сыра сулугуни, яйца и масла.',
@@ -71,7 +63,7 @@ let dishes = [
         id: Math.random(),
         menuImg: "../img/massiveDishes/Low-calorie\ lunch\ with\ yogurt.svg",
         bigImg: "https://recfood.ru/wp-content/uploads/2019/12/cezar-salat-e1508495161510.jpg",
-        name: "Ланч низкокалорийный",
+        name: "Ланч низко-калорийный",
         title: 'Греческий йогурт с ягодами, 3 хинкали традиционных, салат цезарь, пирожки с уткой, морс',
         weight: 450,
         price: 1148,
@@ -95,7 +87,7 @@ let dishes = [
         id: Math.random(),
         menuImg: "../img/massiveDishes/Hamburger\ patty\ with\ potatoes\ in\ a\ rustic\ way.svg",
         bigImg: "https://ferum55.ru/wp-content/uploads/2021/05/bifshteks-solsberi-s-fermerskim-kartofelem-.jpg",
-        name: "Котлета с картофелем по-деревенски",
+        name: "Котлета с картофелем по деревенски",
         title: 'По семейному рецепту: нежнейшее рубленное мясо с экологически чистыми ферме...',
         weight: 350,
         price: 520,
@@ -180,7 +172,7 @@ let dishes = [
         id: Math.random(),
         menuImg: "../img/massiveDishes/Mask\ Group-3.svg",
         bigImg: "https://recfood.ru/wp-content/uploads/2019/12/cezar-salat-e1508495161510.jpg",
-        name: "Ланч низкокалорийный",
+        name: "Ланч низко-калорийный",
         title: 'Греческий йогурт с ягодами, 3 хинкали традиционных, салат цезарь, пирожки с уткой, морс',
         weight: 450,
         price: 1148,
@@ -190,7 +182,7 @@ let dishes = [
     },
     {
         id: Math.random(),
-        menuImg: "../img/massiveDishes/Mask\ Group-4.svg)",
+        menuImg: "../img/massiveDishes/Mask\ Group-4.svg",
         bigImg: "https://hip2go.ru/penza/api2/images/IikoProducts633/199e8a670d-1_1000x.jpg",
         name: "Хачапури по-мегрельски",
         title: 'Невероятно ароматная лепёшка с сыром сулугуни внутри и снаружи',
@@ -202,7 +194,7 @@ let dishes = [
     },
     {
         id: Math.random(),
-        menuImg: "(../img/massiveDishes/Mask\ Group-5.svg)",
+        menuImg: "../img/massiveDishes/Mask\ Group-5.svg",
         bigImg: "https://ferum55.ru/wp-content/uploads/2021/05/bifshteks-solsberi-s-fermerskim-kartofelem-.jpg",
         name: "Котлета с картофелем по-деревенски",
         title: 'По семейному рецепту: нежнейшее рубленное мясо с экологически чистыми ферме...',
@@ -232,6 +224,14 @@ let dishes = [
     },
 
 ]
+let hot_arrow = document.querySelector("#hot_arrow")
+let hot_arrow_box = document.querySelector(".hot_arrow_box")
+// let hotdishes = document.querySelector(".hotdishes")
+// let blackBox = document.querySelector(".blackBox")
+let first_hot_click = document.querySelector(".first_hot_click")
+let about_popular_dishes = document.querySelector(".about_popular_dishes")
+let basket_counter = document.querySelector(".basket_counter")
+let count = 0
 
 let categories = [
     {
@@ -265,6 +265,7 @@ first_hot_click.onclick = () => {
 
 
 
+
 for (let item of categories) {
     for (let dish of item.hot) {
         let a = document.createElement("a");
@@ -273,6 +274,7 @@ for (let item of categories) {
         hot_arrow_box.append(a);
     }
 }
+
 
 // function reload(dishes) {
     for (let item of dishes) {
@@ -284,6 +286,7 @@ for (let item of categories) {
         let p_w_bs = document.createElement("span")
         let price = document.createElement("p")
         let sec_img = document.createElement("img")
+        let inshoper = document.createElement("div")
 
         dish.classList.add("dish")
         ab_dish.classList.add("ab_dish")
@@ -292,18 +295,72 @@ for (let item of categories) {
         text_of_salmon.classList.add("text_of_salmon")
         p_w_bs.classList.add("p_w_bs")
         price.classList.add("price")
+        inshoper.classList.add("inshoper")
 
         first_img.src = item.menuImg
-        sec_img.src = "./img/basket.svg"
+        sec_img.src = "../img/basket.svg"
 
-        ab_dish.innerHTML = item.name
-        weight.innerHTML = item.weight
+        ab_dish.innerHTML = item.name 
+        weight.innerHTML = item.weight + "г"
         text_of_salmon.innerHTML = item.title
-        price.innerHTML = item.price
+        price.innerHTML = item.price + " ₽"
 
         about_popular_dishes.append(dish)
-        dish.append(first_img, ab_dish, weight, text_of_salmon, p_w_bs)
+        dish.append(first_img, ab_dish, weight, text_of_salmon, p_w_bs,)
         p_w_bs.append(price, sec_img)
+
+        sec_img.onclick = () => {
+            basket_counter.classList.remove("hidden")
+            
+            count++
+            basket_counter.innerHTML = count
+
+        }
     }
+
+    // let grids_of_hot_dishes_slide = document.querySelector(".grids_of_hot_dishes_slide")
+
+    // for(let item of categories.hot){
+    //     let dish = document.createElement("div")
+    //     let first_img = document.createElement("img")
+    //     let ab_dish = document.createElement("h2")
+    //     let weight = document.createElement("div")
+    //     let text_of_salmon = document.createElement("p")
+    //     let p_w_bs = document.createElement("span")
+    //     let price = document.createElement("p")
+    //     let sec_img = document.createElement("img")
+    //     let inshoper = document.createElement("div")
+
+    //     dish.classList.add("dish")
+    //     ab_dish.classList.add("ab_dish")
+    //     ab_dish.id = "dish_of_menu"
+    //     weight.classList.add("weight")
+    //     text_of_salmon.classList.add("text_of_salmon")
+    //     p_w_bs.classList.add("p_w_bs")
+    //     price.classList.add("price")
+    //     inshoper.classList.add("inshoper")
+
+    //     first_img.src = item.menuImg
+    //     sec_img.src = "../img/basket.svg"
+
+    //     ab_dish.innerHTML = item.name 
+    //     weight.innerHTML = item.weight + "г"
+    //     text_of_salmon.innerHTML = item.title
+    //     price.innerHTML = item.price + " ₽"
+
+    //     grids_of_hot_dishes_slide.append(dish)
+    //     dish.append(first_img, ab_dish, weight, text_of_salmon, p_w_bs,)
+    //     p_w_bs.append(price, sec_img)
+
+    //     sec_img.onclick = () => {
+    //         basket_counter.classList.remove("hidden")
+            
+    //         count++
+    //         basket_counter.innerHTML = count
+
+    //     }
+    // }
+console.log(categories.hot);
+
 // }
 // reload()
